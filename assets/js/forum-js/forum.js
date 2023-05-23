bebb08ddd25701448cb18e5a739213d48b1c88f8
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-05-22 19:01:45 -0600

    foto y estilos css

--- assets/js/forum-js/forum.js
+++ assets/js/forum-js/forum.js
@@ -1,80 +1,106 @@
-// Función para manejar el evento de clic en el botón "Agregar publicación"
+// Función para manejar el evento de clic en el botón "Publicar"
 function addPost() {
-    const postInput = document.getElementById("post-input").value.trim();
-  
-    if (postInput === "") {
-      alert("Favor de publicar algo.");
-      return;
-    }
-  
-    // Crear un contenedor para la publicación
-    const postContainer = document.createElement("div");
-    postContainer.classList.add("post-container");
-  
-    // Crear un elemento de texto para la publicación
-    const postText = document.createElement("h3");
-    postText.textContent = postInput;
-    postText.classList.add("post-text"); // Agregar clase al elemento de texto de la publicación
-    postContainer.appendChild(postText);
-  
-    // Crear un formulario para las respuestas
-    const replyForm = document.createElement("div");
-    replyForm.classList.add("reply__form");
-  
-    // Crear un campo de entrada para las respuestas
-    const replyInput = document.createElement("input");
-    replyInput.type = "text";
-    replyInput.placeholder = "¡Comenta algo!";
-    replyForm.appendChild(replyInput);
-  
-    // Crear un botón para enviar las respuestas
-    const replyButton = document.createElement("button");
-    replyButton.classList.add("reply-btn");
-    replyButton.textContent = "Comenta";
-    replyButton.addEventListener("click", addReply);
-    replyForm.appendChild(replyButton);
-  
-    // Agregar el formulario de respuestas al contenedor de la publicación
-    postContainer.appendChild(replyForm);
-  
-    // Obtener el contenedor de la pared (wall)
-    const wallContainer = document.querySelector(".wall__container");
-    wallContainer.appendChild(postContainer);
-  
-    // Limpiar el campo de entrada de la publicación
-    document.getElementById("post-input").value = "";
+  const postInput = document.getElementById("post-input").value.trim();
+
+  if (postInput === "") {
+    alert("Favor de publicar algo.");
+    return;
   }
-  
-  // Función para manejar el evento de clic en el botón "Responder"
-  function addReply(event) {
-    const replyInput =
-      event.target.parentNode.querySelector("input[type='text']");
-    const replyText = replyInput.value.trim();
-  
-    if (replyText === "") {
-      alert("Favor de contestar algo.");
-      return;
-    }
-  
-    // Crear un contenedor para la respuesta
-    const replyContainer = document.createElement("div");
-    replyContainer.classList.add("reply-container");
-  
-    // Crear un elemento de texto para la respuesta
-    const replyTextElement = document.createElement("p");
-    replyTextElement.textContent = replyText;
-    replyTextElement.classList.add("reply-text"); // Agregar clase al elemento de texto de la respuesta
-    replyContainer.appendChild(replyTextElement);
-  
-    // Obtener el contenedor de la publicación a la cual se está respondiendo
-    const postContainer = event.target.parentNode.parentNode;
-    postContainer.appendChild(replyContainer);
-  
-    // Limpiar el campo de entrada de respuesta
-    replyInput.value = "";
+
+  // Crear un contenedor para la publicación
+  const postContainer = document.createElement("div");
+  postContainer.classList.add("post-container");
+
+  // Crear un elemento de imagen para la publicacion
+  const postImage = document.createElement("img");
+  postImage.src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp";
+  postImage.classList.add("rounded-circle"); // Agregar clase al elemento de imagen de la publicación
+  postImage.classList.add("d-flex");
+  postImage.classList.add("align-self-start");
+  postImage.classList.add("me-3");
+  postImage.classList.add("shadow-1-strong");
+  postContainer.appendChild(postImage); // Agregar la imagen al contenedor de publicaciones
+
+  // Crear un elemento de texto para la publicación
+  const postText = document.createElement("h3");
+  postText.textContent = postInput;
+  postText.classList.add("post-text"); // Agregar clase al elemento de texto de la publicación
+  postContainer.appendChild(postText); //Agregando el h3 al div del post container
+
+  //Crear un contenedor para las respuestas de los usuarios
+  const listOfAnswer = document.createElement("div");
+  listOfAnswer.classList.add("users_reply__form");
+
+  postContainer.appendChild(listOfAnswer);
+
+  // Crear un formulario para las respuestas
+  const replyForm = document.createElement("div");
+  replyForm.classList.add("reply__form");
+    
+  // Crear un campo de entrada para las respuestas
+  const replyInput = document.createElement("input");
+  replyInput.type = "text";
+  replyInput.placeholder = "¡Comenta algo!";
+  replyForm.appendChild(replyInput);
+    
+  // Crear un botón para enviar las respuestas
+  const replyButton = document.createElement("button");
+  replyButton.classList.add("reply-btn");
+  replyButton.textContent = "Comenta";
+  replyButton.addEventListener("click", addReply);
+  replyForm.appendChild(replyButton);
+
+  // Agregar el formulario de respuestas al contenedor de la publicación
+  postContainer.appendChild(replyForm);
+
+  // Obtener el contenedor de la pared (wall)
+  const wallContainer = document.querySelector(".wall__container");
+  wallContainer.appendChild(postContainer);
+
+  // Limpiar el campo de entrada de la publicación
+  document.getElementById("post-input").value = "";
+}
+
+// Función para manejar el evento de clic en el botón "Responder"
+function addReply(event) {
+  const replyInput =
+    event.target.parentNode.querySelector("input[type='text']");
+  const replyText = replyInput.value.trim();
+
+  if (replyText === "") {
+    alert("Favor de contestar algo.");
+    return;
   }
-  
-  // Agregar un event listener al botón "Agregar publicación"
-  const addPostButton = document.getElementById("add-post-btn");
-  addPostButton.addEventListener("click", addPost);
-  
+
+  // Crear un contenedor para la respuesta
+  const replyContainer = document.createElement("div");
+  replyContainer.classList.add("reply-container");
+
+  // Crear un elemento de imagen para la respuesta
+  const postImage = document.createElement("img");
+  postImage.src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp"; // Reemplaza "ruta_de_la_imagen.jpg" por la ruta de la imagen que deseas mostrar
+  postImage.classList.add("rounded-circle"); // Agregar clase al elemento de imagen de la publicación
+  postImage.classList.add("d-flex");
+  postImage.classList.add("align-self-start");
+  postImage.classList.add("me-3");
+  postImage.classList.add("shadow-1-strong");
+  replyContainer.appendChild(postImage); // Agregar la imagen al contenedor de publicaciones
+
+  // Crear un elemento de texto para la respuesta
+  const replyTextElement = document.createElement("p");
+  replyTextElement.textContent = replyText;
+  replyTextElement.classList.add("reply-text"); // Agregar clase al elemento de texto de la respuesta
+  replyContainer.appendChild(replyTextElement);
+
+
+// Obtener el contenedor de la publicación a la cual se está respondiendo
+  const postContainer = event.target.parentNode.parentNode;
+  postContainer.appendChild(replyContainer); 
+
+  // Limpiar el campo de entrada de respuesta
+  replyInput.value = "";
+}
+
+// Agregar un event listener al botón "Agregar publicación"
+const addPostButton = document.getElementById("add-post-btn");
+addPostButton.addEventListener("click", addPost);
