02e5c0971f3d4189620fb78a6e09741f9bb02181
Nicolas <walsergomez@gmail.com>
2023-05-26 15:45:28 -0600

    ci: get from local storage

--- assets/js/forum-js/forum2.js
+++ assets/js/forum-js/forum2.js
@@ -223,3 +223,135 @@ function showItems() {
 }
 
 document.addEventListener("DOMContentLoaded", showItems);
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Function to store the information in local storage
+  function saveDataToLocalStorage(data) {
+    localStorage.setItem("forum1Posts", JSON.stringify(data));
+  }
+
+  // Function to retrieve the information from local storage
+  function getDataFromLocalStorage() {
+    const storedData = localStorage.getItem("forum1Posts");
+    return JSON.parse(storedData);
+  }
+
+  // Function to populate the wall__container with the retrieved data
+  function populateWallContainer(data) {
+    const wallContainer = document.querySelector(".wall__container");
+    wallContainer.innerHTML = "";
+
+    data.postData.forEach((postData) => {
+      const postContainer = document.createElement("div");
+      postContainer.classList.add("post-container");
+      postContainer.setAttribute("data-postId", postData.postDataId);
+
+      postData.postHeader.forEach((postHeader) => {
+        const postHeaderUser = document.createElement("div");
+        postHeaderUser.classList.add("general-post-info");
+
+        const postContentDiv = document.createElement("div");
+        postContentDiv.classList.add("post-content");
+
+        const postImage = document.createElement("img");
+        postImage.src =
+          "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp";
+        postImage.classList.add("rounded-circle");
+        postImage.classList.add("me-3");
+        postImage.classList.add("shadow-1-strong");
+        postImage.style.width = "60px";
+        postImage.style.height = "60px";
+
+        const nameElement = document.createElement("h3");
+        nameElement.textContent = postHeader["post-header-name"];
+        nameElement.classList.add("post-name");
+
+        const postDate = document.createElement("p");
+        postDate.textContent = postHeader["post-header-date"];
+        postDate.classList.add("post-date");
+
+        postContentDiv.appendChild(postImage);
+        postContentDiv.appendChild(nameElement);
+        postContentDiv.appendChild(postDate);
+
+        postHeaderUser.appendChild(postContentDiv);
+
+        const postTextDiv = document.createElement("div");
+        postTextDiv.classList.add("posttextdiv");
+
+        const postTextElement = document.createElement("p");
+        postTextElement.textContent = postHeader["post-header-text"];
+        postTextElement.classList.add("post-text");
+        postTextDiv.appendChild(postTextElement);
+
+        postHeaderUser.appendChild(postTextDiv);
+
+        postContainer.appendChild(postHeaderUser);
+      });
+
+      postData.replyData.forEach((replyData) => {
+        const replyContainer = document.createElement("div");
+        replyContainer.classList.add("reply-container");
+
+        const replyContentDiv = document.createElement("div");
+        replyContentDiv.classList.add("reply-content");
+
+        const replyImage = document.createElement("img");
+        replyImage.src =
+          "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp";
+        replyImage.classList.add("rounded-circle");
+        replyImage.classList.add("me-3");
+        replyImage.classList.add("shadow-1-strong");
+        replyImage.style.width = "60px";
+        replyImage.style.height = "60px";
+
+        const nameElement = document.createElement("h3");
+        nameElement.textContent = replyData["reply-name"];
+        nameElement.classList.add("reply-name");
+
+        const replyDate = document.createElement("p");
+        replyDate.textContent = replyData["reply-date"];
+        replyDate.classList.add("reply-date");
+
+        replyContentDiv.appendChild(replyImage);
+        replyContentDiv.appendChild(nameElement);
+        replyContentDiv.appendChild(replyDate);
+
+        const textReplyDiv = document.createElement("div");
+        textReplyDiv.classList.add("text-reply");
+
+        const replyTextElement = document.createElement("p");
+        replyTextElement.textContent = replyData["reply-text"];
+        replyTextElement.classList.add("reply-text");
+        textReplyDiv.appendChild(replyTextElement);
+
+        replyContainer.appendChild(replyContentDiv);
+        replyContainer.appendChild(textReplyDiv);
+
+        postContainer.appendChild(replyContainer);
+      });
+
+      wallContainer.appendChild(postContainer);
+    });
+  }
+
+  // Fetch data from local storage and populate the wall__container
+  const storedData = getDataFromLocalStorage();
+  if (storedData) {
+    populateWallContainer(storedData);
+  }
+
+  // Axios request to post the data to the local storage
+  axios
+    .get("/getforum1")
+    .then((response) => {
+      const data = response.data;
+      saveDataToLocalStorage(data);
+      populateWallContainer(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// Attempt 2
