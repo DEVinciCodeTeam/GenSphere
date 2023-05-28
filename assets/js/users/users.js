34185f76cd5fdc984b955370ae1009a9616f323b
andrealar30 <dreaguilar098@gmail.com>
2023-05-28 13:48:21 -0600

    Jalando datos de perfil editable a localStorage

--- assets/js/users/users.js
+++ assets/js/users/users.js
@@ -1,9 +1,10 @@
 // Objeto que almacena todos los objetos de datos creados dentro de las funciones
-console.log("script users esta funcionando");
-const allUsers = { users: {} };
-let usersCount = 0;
+let generalUsersCount = 0;
+let usersCountByCohorte = {};
 
+const allUsers = {};
 const signUpForm = document.getElementById("signUpForm");
+
 
 signUpForm.onsubmit = function(e) {
 
@@ -12,310 +13,72 @@ signUpForm.onsubmit = function(e) {
   const getUserEmail = document.getElementById("userEmail").value.trim();
   const getUserCohorte = document.getElementById("userCohorte").value.trim();
   const getUserPassword = document.getElementById("userPassword").value.trim();
-  console.log(getUserName, getUserEmail, getUserCohorte, getUserPassword);
-
-  // Insertar if de verificacion (si es el primer ususario o no)
-  // Verificar si alguien ya de esa corte o no (en caso de que no crear la clave en allUsers)
-
-
 
   if (getUserName == "" || getUserEmail == "" || getUserCohorte == "" || getUserPassword == "") {
     alert("Porfavor, completa todos los campos")
   } else {
-    allUsers.users[usersCount++] = {
-      userName: getUserName,
-      userEmail: getUserEmail,
-      userCohorte: getUserCohorte,
-      userPassword: getUserPassword
+    if (!allUsers[getUserEmail]) {
+      console.log(getUserName, getUserEmail, getUserCohorte, getUserPassword);
+
+      if (!usersCountByCohorte[getUserCohorte]) {
+        usersCountByCohorte[getUserCohorte] = 0
+      }
+
+      allUsers[getUserEmail] = {
+        userId: generalUsersCount++,
+        usertIdCohorte: usersCountByCohorte[getUserCohorte]++,
+        userName: getUserName,
+        userEmail: getUserEmail,
+        userCohorte: getUserCohorte,
+        userPassword: getUserPassword
+      }
+      localStorage.setItem("allUsers", JSON.stringify(allUsers))
+    } else {
+      const signUpFormContainer = document.getElementById("signUpFormContainer");
+      alert("Esta cuenta de correo ya ha sido registrada previamente\nIngresa un correo distinto, por favor.")
+
     }
   }
+
 }
 
 
 const signInForm = document.getElementById("signInForm");
-let identifiedUser;
+
 signInForm.onsubmit = function(e) {
   e.preventDefault()
   const getUserEmail = document.getElementById("userEmailLogin").value.trim();
-  const getUserCohorte = document.getElementById("userCohorteLogin").value.trim();
   const getUserPassword = document.getElementById("userPasswordLogin").value.trim();
 
-  // Sacar la corte
+  const allUsers = JSON.parse(localStorage.getItem("allUsers"));
 
-  let index = 0;
-
-  const usersNum = Object.keys(allUsers.users).length;
-
-  while (index != usersNum && allUsers.users[index].userEmail != getUserEmail) {
-    console.log(allUsers.users[index].userEmail != getUserEmail);
-    index++;
-  }
-
-  if (index == usersNum) {
-    alert("El correo no esta registrado");
-  }
-
-
-  identifiedUser = allUsers.users[index];
-  if (getUserCohorte == identifiedUser.userCohorte && getUserPassword == identifiedUser.userPassword) {
-    console.log("Entraste!")
+  if (!allUsers[getUserEmail]) {
+    // alert("El correo no esta registrado, por favor crea una cuenta en GenSphere.");
+    restablecerBoton("");
   } else {
-    if (getUserCohorte != identifiedUser.userCohorte) {
-      alert("La cohorte no coincide con el correo que proporcionaste");
-    } else if (getUserPassword != identifiedUser.userPassword) {
+
+    if (getUserPassword == allUsers[getUserEmail].userPassword) {
+      console.log("Entraste!")
+    } else {
       alert("La contraseña no coincide con el correo que proporcionaste");
     }
   }
 
 }
 
-// Obtener el perfil editable
-const getUserName = document.getElementById("editUserName");
-const getUserCohorte = document.getElementById("editUserCohorte");
-const getUserEmail = document.getElementById("editUserEmail");
 
+const quitarBoton = (elementId) => {
+  const buttonRef = document.getElementById(elementId);
+  buttonRef.style.display = "none";
+}
 
-getUserName.innerHTML = identifiedUser;
+const ocultarBoton = (elementId) => {
+  const buttonRef = document.getElementById(elementId);
+  buttonRef.style.visibility = "hidden";
+}
 
-// Obtener los datos del usuario registrado desde allUsers
-const name = allUsers.users[editUserName];
-const cohorte = allUsers.users[editUserCohorte];
-const email = allUsers.users[editUserEmail];
-
-// Actualizar los campos del perfil editable con los datos del usuario
-profileForm.userName.value = name.userName;
-profileForm.userEmail.value = usuario.userEmail;
-profileForm.userCohorte.value = usuario.userCohorte;
-
-
-
-/* const editAllUsers = allUsers.users[index];
-const getUserName = document.getElementById ("editUserName").value.trim();
-const getUserCohorte = document.getElementById ("editUserCohorte").value.trim();
-const getUserEmail = document.getElementById ("editUserEmail").value.trim();
-console.log(getUserName, getUserCohorte, getUserEmail);
- */
-
-
-  // let postDataId = null;
-  // let postDataIdCounter = 1;
-  // const postHeader = {};
-
-  // //Iteradores
-  // let postHeaderId = null;
-  // let postHeaderIdCounter = 1;
-  // let postReplyIdCounter = 1;
-
-  // // Función para manejar el evento de clic en el botón "Agregar publicación"
-  // function addPost() {
-  //   const postInput = document.getElementById("post-input").value.trim();
-
-  //   if (postInput === "") {
-  //     alert("Favor de publicar algo.");
-  //     return;
-  //   }
-
-  //   // Crear un contenedor para la publicación
-  //   const postContainer = document.createElement("div");
-  //   postContainer.classList.add("post-container");
-  //   postContainer.setAttribute("data-postId", postHeaderIdCounter);
-
-  //   // Crear un contenedor para el encabezado de la publicación
-  //   const postHeaderUser = document.createElement("div");
-  //   postHeaderUser.classList.add("general-post-info");
-
-  //   // Crear un div para el contenido de la publicación
-  //   const postContentDiv = document.createElement("div");
-  //   postContentDiv.classList.add("post-content");
-
-  //   // Crear un elemento de imagen para la publicación
-  //   const postImage = document.createElement("img");
-  //   postImage.src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp";
-  //   postImage.classList.add("rounded-circle");
-  //   postImage.classList.add("me-3");
-  //   postImage.classList.add("shadow-1-strong");
-  //   postImage.style.width = "60px";
-  //   postImage.style.height = "60px";
-
-  //   // Crear un elemento para el nombre
-  //   const nameElement = document.createElement("h3");
-  //   nameElement.textContent = "John Doe";
-  //   nameElement.classList.add("post-name");
-
-  //   // Crear un elemento para la fecha
-  //   const postDate = document.createElement("p");
-  //   const currentDate = new Date();
-  //   postDate.textContent = currentDate.toLocaleDateString();
-  //   postDate.classList.add("post-date");
-
-  //   postContentDiv.appendChild(postImage);
-  //   postContentDiv.appendChild(nameElement);
-  //   postContentDiv.appendChild(postDate);
-
-  //   postHeaderUser.appendChild(postContentDiv);
-
-  //   // Crear un div para el texto de la publicación
-  //   const postTextDiv = document.createElement("div");
-  //   postTextDiv.classList.add("posttextdiv");
-
-  //   // Crear un elemento para el texto de la publicación
-  //   const postTextElement = document.createElement("p");
-  //   postTextElement.textContent = postInput;
-  //   postTextElement.classList.add("post-text");
-  //   postTextDiv.appendChild(postTextElement);
-
-  //   postHeaderUser.appendChild(postTextDiv);
-
-  //   postContainer.appendChild(postHeaderUser);
-
-  //   // Crear un contenedor para las respuestas de los usuarios
-  //   const listOfAnswer = document.createElement("div");
-  //   listOfAnswer.classList.add("users_reply__form");
-
-  //   // Crear un formulario para las respuestas
-  //   const replyForm = document.createElement("div");
-  //   replyForm.classList.add("reply__form");
-
-  //   // Crear un campo de entrada para las respuestas
-  //   const replyInput = document.createElement("input");
-  //   replyInput.type = "text";
-  //   replyInput.placeholder = "¡Comenta algo!";
-  //   replyForm.appendChild(replyInput);
-
-  //   // Crear un botón para enviar las respuestas
-  //   const replyButton = document.createElement("button");
-  //   replyButton.classList.add("reply-btn");
-  //   replyButton.textContent = "Comenta";
-  //   replyButton.addEventListener("click", addReply);
-  //   replyForm.appendChild(replyButton);
-
-  //   postContainer.appendChild(listOfAnswer);
-  //   postContainer.appendChild(replyForm);
-
-  //   const wallContainer = document.querySelector(".wall__container");
-  //   wallContainer.appendChild(postContainer);
-
-  //   document.getElementById("post-input").value = "";
-
-  //   let postHeaderId = postHeaderIdCounter++;
-
-  //   const postHeader = {
-  //     postHeaderId,
-  //     "post-header-name": nameElement.textContent,
-  //     "post-header-date": postDate.textContent,
-  //     "post-header-text": postInput,
-  //   };
-
-  //   let postDataId = postDataIdCounter++;
-
-  //   const postData = {
-  //     postDataId,
-  //     postHeader: [],
-  //     replyData: [],
-  //   }; //Quitar
-
-  //   postData.postHeader.push(postHeader);
-
-  //   allData.postData.push(postData);
-
-  //   console.clear();
-
-  //   //Guardar en Local Storage
-  //   appendObjectToLocalStorage(allData);
-  // }
-
-  // function addReply(event) {
-  //   const replyInput =
-  //     event.target.parentNode.querySelector("input[type='text']");
-  //   const replyText = replyInput.value.trim();
-
-  //   if (replyText === "") {
-  //     alert("Por favor comenta algo.");
-  //     return;
-  //   }
-
-  //   // Crear un contenedor para la respuesta
-  //   const replyContainer = document.createElement("div");
-  //   replyContainer.classList.add("reply-container");
-
-  //   const replyContentDiv = document.createElement("div");
-  //   replyContentDiv.classList.add("reply-content");
-
-  //   const replyImage = document.createElement("img");
-  //   replyImage.src = "https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp";
-  //   replyImage.classList.add("rounded-circle");
-  //   replyImage.classList.add("me-3");
-  //   replyImage.classList.add("shadow-1-strong");
-  //   replyImage.style.width = "60px";
-  //   replyImage.style.height = "60px";
-
-  //   const nameElement = document.createElement("h3");
-  //   nameElement.textContent = "Jane Doe";
-  //   nameElement.classList.add("reply-name");
-
-  //   const replyDate = document.createElement("p");
-  //   const currentDate = new Date();
-  //   replyDate.textContent = currentDate.toLocaleDateString();
-  //   replyDate.classList.add("reply-date");
-
-  //   replyContentDiv.appendChild(replyImage);
-  //   replyContentDiv.appendChild(nameElement);
-  //   replyContentDiv.appendChild(replyDate);
-
-  //   const textReplyDiv = document.createElement("div");
-  //   textReplyDiv.classList.add("text-reply");
-
-  //   const replyTextElement = document.createElement("p");
-  //   replyTextElement.textContent = replyText;
-  //   replyTextElement.classList.add("reply-text");
-  //   textReplyDiv.appendChild(replyTextElement);
-
-  //   replyContainer.appendChild(replyContentDiv);
-  //   replyContainer.appendChild(textReplyDiv);
-
-  //   const postContainer = event.target.parentNode.parentNode;
-  //   const listOfAnswer = postContainer.querySelector(".users_reply__form");
-  //   listOfAnswer.appendChild(replyContainer);
-
-  //   replyInput.value = "";
-
-  //   const replyId = postReplyIdCounter++;
-
-  //   const replyData = {
-  //     replyId,
-  //     postHeaderId: postContainer.getAttribute("data-postId"),
-  //     "reply-name": nameElement.textContent,
-  //     "reply-date": replyDate.textContent,
-  //     "reply-text": replyText,
-  //   };
-
-  //   let idHeader = postContainer.getAttribute("data-postId");
-  //   allData.postData[idHeader - 1].replyData.push(replyData);
-
-  //   //Guardar en Local Storage
-  //   appendObjectToLocalStorage(allData);
-
-  //   allData.postData[idHeader - 1].replyData.push(replyData);
-  // }
-
-  // // Add an event listener to the "Agregar publicación" button
-  // const addPostButton = document.getElementById("add-post-btn");
-  // addPostButton.addEventListener("click", addPost);
-
-  // //Funcion para guardar la informacion en Local Storage.
-  // function appendObjectToLocalStorage(allData) {
-  //   const element = allData;
-  //   localStorage.setItem("forum1Posts", JSON.stringify(element));
-  // }
-  // // Lista aparece en orden con a animación
-  // const listItems = document.querySelectorAll(".list-animation");
-
-  // function showItems() {
-  //   listItems.forEach((item, index) => {
-  //     setTimeout(() => {
-  //       item.classList.add("fade-in");
-  //     }, index * 350);
-  //   });
-  // }
-
-  // document.addEventListener("DOMContentLoaded", showItems); 
+const restablecerBoton = (elementId) => {
+  const buttonRef = document.getElementById(elementId);
+  buttonRef.style.display = "inline";
+  buttonRef.style.visibility = "visible";
+}
